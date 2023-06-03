1546b637d9ecc79a3a7bacf60be9cc61d8e8bab5
Josnell09 <134009051+Josnell09@users.noreply.github.com>
2023-06-02 20:26:08 -0400

    Add files via upload

---
+++ ejemplo 1/main.c
@@ -0,0 +1,17 @@
+
+#include <stdlib.h>
+#include <stdio.h>                  /* Promedio curso.El programa, al recibir como dato el promedio
+                                    de un alumno en un curso➥universitario, escribe aprobado si su
+                                    promedio es mayor o igual a 6.PRO: variable de tipo real. */
+
+void main(void)
+{
+    float PRO;
+    printf("ingrese el promedio del alumno: ");
+    scanf("%f", &PRO);
+    if(PRO >= 6)printf("\nAprobado");
+
+    if(PRO <= 6)printf("\nreprobado");
+
+    }
+
