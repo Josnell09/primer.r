b5cc892114a62059fe67b60be3c25072199a86ff
Josnell09 <134009051+Josnell09@users.noreply.github.com>
2023-06-09 15:52:27 -0400

    Add files via upload

---
+++ ejemplo 1/programa cap 3/P3.8.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+# include <stdio.h>
+/* Factorial.El programa calcula el factorial de un número entero.
+ FAC, I, NUM: variables de tipo entero. */
+void main(void)
+{
+    int I, NUM;
+    long FAC;
+    printf("\nIngrese el número: ");
+    scanf("%d", &NUM);
+    if(NUM >= 0)
+    {
+        FAC = 1;
+        for(I=1; I <= NUM; I++)
+FAC *= I;
+ printf("\El factorial de %d es: %ld", NUM, FAC);
+ } else
+ printf("\nError en el dato ingresado");
+ }
